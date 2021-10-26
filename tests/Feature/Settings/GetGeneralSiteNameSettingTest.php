441c9c8fb10a96370a1b900c8acf00d38aa70d7b
Alina <alina.kessler@prowect.com>
2021-10-27 00:49:09 +0200

    GitHub Workflow with migrate

--- tests/Feature/Settings/GetGeneralSiteNameSettingTest.php
+++ tests/Feature/Settings/GetGeneralSiteNameSettingTest.php
@@ -2,11 +2,14 @@
 
 namespace ProwectCMS\Core\Tests\Feature\Settings;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use ProwectCMS\Core\Settings\GeneralSettings;
 use ProwectCMS\Core\Tests\TestCase;
 
 class GetGeneralSiteNameSettingTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function testGetGeneralSiteNameSetting()
     {
         $settings = app(GeneralSettings::class);
